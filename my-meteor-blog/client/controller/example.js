44ff5da68228bc4794b6e1a907bc4e3a461b2690
webdevid <dodihidayatullah@gmail.com>
2015-12-14 06:17:50 +0700

    adding sample event with session

--- my-meteor-blog/client/controller/example.js
+++ my-meteor-blog/client/controller/example.js
@@ -6,6 +6,13 @@ Template.contextExample.rendered = function(){
 Template.contextExample.helpers({
 	logContext:function(){
 		console.log('Context Log helpers',this);
+		return Session.get('randomNumber');
+	}
+})
+
+Template.contextExample.events({
+	'click button':function(e,Template){
+		Session.set('randomNumber',Math.random(0,999));
 	}
 })
 
